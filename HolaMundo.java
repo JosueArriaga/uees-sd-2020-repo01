34ad5a922ec01737284dab456d2da9b6362e0787
JosueArriaga <70612051+JosueArriaga@users.noreply.github.com>
2020-09-01 19:09:46 -0500

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,7 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {		
-		System.out.println("Hola mundo! Este es mi primer repositorio en GitHub");
+		System.out.println("Hola mundo! Este es mi primer repositorio en GitHub.");
 	}
 
 }
